1ef9afea771b12ab92d9493f0e41e8a2017c7e02
JonMoll <jonathan.mollapaza@ucsp.edu.pe>
2017-06-21 17:34:30 -0500

    Anadiendo codigo para abrir el script al iniciar el SO

--- tarea.sh
+++ tarea.sh
@@ -1,1 +1,24 @@
+# Ejecutar el script cada vez que se inicie Ubuntu:
+# 1. Le damos permiso de ejecucion:
+#    sudo chmod +x tarea.sh
+# 2. Abrimos el fichero rc.local:
+#    sudo nano /etc/rc.local
+# 3. Agregamos la instruccion para ejecutar el script (con KDE es konsole):
+#    sh /home/jonathan/git/SO-Tarea/tarea.sh
+#    exit 0
+#    * Nota: el script no se ejectuta en el GUI si no en la consola del OS
+#
+# Ejecutar script cada vez que inicie KDE Neon
+# 1. Abrir el menu de autoarranque
+# 2. Anadir script:
+#    konsole -e sh /home/jonathan/git/SO-Tarea/tarea.sh
 
+echo -n "¿Desea iniciar el script? s/n: "
+read iniciar
+
+if [ "$iniciar" = 's' ]; then
+    echo "Iniciando..."
+    read salir
+else
+    echo "Usted ha salido."
+fi;
